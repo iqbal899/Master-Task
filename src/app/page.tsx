0bc98723721fc58f84427342ed7081eeb73ba392
iqbal899 <jinnatuliqbal99@gmail.com>
2026-02-07 14:49:27 +0530

    created session middleware  and logout route handled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
-import {Button} from "@/components/ui/button"; 
+"use client";
+import { Button } from "@/components/ui/button"
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return (
-    <div className="flex gap-4">
-      <Button variant="primary" >
-        Primary 
+  const router = useRouter()
+  const { data,isLoading } = useCurrent();
+  const { mutate } =useLogout()
+
+  useEffect(() => {
+    if(!data && !isLoading ){
+       router.push("/sign-in")
+    }
+  },[data])
+
+  return(
+    <div>
+      Only visible to authorized users
+      <Button onClick={() => mutate()}>
+        Logout
       </Button>
-      <Button variant="secondary">
-        Secondary
-      </Button>
-      <Button variant="destructive">
-        Destructive
-      </Button>
-      <Button variant= "ghost">
-        Ghost
-      </Button>
-      <Button variant= "muted">
-        Muted
-      </Button>
-      <Button variant={"outline"}>
-        Outline
-      </Button>
-      <Button variant={"teritary"}>
-        Teritary
-      </Button>  
     </div>
-  );
+  )
 }
