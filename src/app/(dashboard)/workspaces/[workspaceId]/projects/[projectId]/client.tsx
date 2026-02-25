8d7cbe58ca65ff09bca7f432e24e3f7b25846593
iqbal899 <jinnatuliqbal99@gmail.com>
2026-02-25 20:52:31 +0530

    project analytics managed

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -11,16 +11,21 @@ import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher
 import { Button } from "@/components/ui/button";
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
+import { useGetProjectAnalytics } from "@/features/projects/api/use-get-project-analytics";
+import { Analytics } from "@/components/analytics";
 
 export const ProjectIdClient = () => {
   const projectId = useProjectId();
-  const { data, isLoading } = useGetProject({ projectId });
+  const { data: project, isLoading: isLoadingProject } = useGetProject({ projectId });
+  const { data: analytics, isLoading: isLoadingAnalytics } = useGetProjectAnalytics({ projectId });
+
+  const isLoading = isLoadingProject || isLoadingAnalytics
 
   if (isLoading) {
     return <PageLoader />;
   }
 
-  if (!data) {
+  if (!project ) {
     return <PageError message="Project not found" />;
   }
 
@@ -29,23 +34,26 @@ export const ProjectIdClient = () => {
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-x-2">
           <ProjectAvatar
-            name={data.name}
-            image={data.imageUrl}
+            name={project.name}
+            image={project.imageUrl}
             className="size-8"
           />
-          <p className="text-lg font-semibold">{data.name}</p>
+          <p className="text-lg font-semibold">{project.name}</p>
         </div>
 
         <Button variant="secondary" size="sm" asChild>
           <Link
-            href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}
+            href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}
           >
             <PencilIcon className="size-4 mr-2" />
             Edit Project
           </Link>
         </Button>
       </div>
-
+      {analytics ? (
+        //reusable component from components
+        <Analytics data={analytics} />
+      ) : null}
       <TaskViewSwitcher hideProjectFilter />
     </div>
   );
