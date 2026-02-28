76e5f8b6b5bffd68352b4e4504919f2621a5461e
iqbal899 <jinnatuliqbal99@gmail.com>
2026-02-28 22:15:37 +0530

    project page refactored

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -17,6 +17,7 @@ import { useCallback } from "react"
 import { TaskStatus } from "../types"
 import { useBulkUpdateTasks } from "../api/use-bulk-update-task"
 import { DataCalendar } from "./data-calendar"
+import { useProjectId } from "@/features/projects/hooks/use-project-id"
 
 interface TaskViewSwitcherProps {
     hideProjectFilter?: boolean
@@ -33,7 +34,9 @@ export const TaskViewSwitcher = ({ hideProjectFilter }:TaskViewSwitcherProps) =>
     const [view, setView] = useQueryState("task-view", {
         defaultValue: "table"
     })
+
     const workspaceId = useWorkspaceId()
+    const paramProjectId = useProjectId()
 
     const { open } =  useCreateTaskModal()
 
@@ -44,7 +47,7 @@ export const TaskViewSwitcher = ({ hideProjectFilter }:TaskViewSwitcherProps) =>
          isLoading: isLoadingTasks
          } = useGetTasks({ 
             workspaceId,
-            projectId,
+            projectId: paramProjectId || projectId,
             assigneeId,
             status,
             dueDate,
