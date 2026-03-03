a0119e9ba163f3028f613ff61e2a232b8cd492ef
iqbal899 <jinnatuliqbal99@gmail.com>
2026-03-03 22:19:56 +0530

    updated oauth.ts

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -6,30 +6,49 @@ import { OAuthProvider } from "node-appwrite";
 
 import { createAdminClient } from "@/lib/appwrite";
 
+function getSuccessRedirect() {
+  const headerList = headers();
+  const origin = headerList.get("origin");
+  const referer = headerList.get("referer");
+
+  if (referer && referer.includes("/join/")) {
+    return referer;
+  }
+
+
+  return `${origin}/oauth`;
+}
+
 export async function signUpWithGithub() {
-	const { account } = await createAdminClient();
+  const { account } = await createAdminClient();
 
-  const origin = headers().get("origin");
-  
-	const redirectUrl = await account.createOAuth2Token(
-		OAuthProvider.Github,
-		`${origin}/oauth`,
-		`${origin}/sign-up`,
-	);
+  const headerList = headers();
+  const origin = headerList.get("origin");
 
-	return redirect(redirectUrl);
-};
+  const successUrl = getSuccessRedirect();
+
+  const redirectUrl = await account.createOAuth2Token(
+    OAuthProvider.Github,
+    successUrl!,
+    `${origin}/sign-in`
+  );
+
+  return redirect(redirectUrl);
+}
 
 export async function signUpWithGoogle() {
-	const { account } = await createAdminClient();
+  const { account } = await createAdminClient();
 
-  const origin = headers().get("origin");
+  const headerList = headers();
+  const origin = headerList.get("origin");
 
-	const redirectUrl = await account.createOAuth2Token(
-		OAuthProvider.Google,
-		`${origin}/oauth`,
-		`${origin}/sign-up`,
-	);
+  const successUrl = getSuccessRedirect();
 
-	return redirect(redirectUrl);
-};
+  const redirectUrl = await account.createOAuth2Token(
+    OAuthProvider.Google,
+    successUrl!,
+    `${origin}/sign-in`
+  );
+
+  return redirect(redirectUrl);
+}
