dcf31cc6a3af0a6436405a6844911fdafbaecb30
iqbal899 <jinnatuliqbal99@gmail.com>
2026-03-06 15:12:56 +0530

    updated and fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 
  
 const inter = Inter({ subsets: ["latin"] });
-export const metadata = {
+export const metadata: Metadata = {
   title: "Master Task",
   description: "A Jira-inspired project management platform for teams to manage tasks, projects, and workflows efficiently.",
 };
